2832acff37441ce2d4f1edf03b772f368d6686ef
mtchdlr <datomtchedluri@gmail.com>
2025-09-08 21:16:46 +0400

    feat(utils/udf_util.py): add the function for extracting start salary and end salary

--- utils/udf_util.py
+++ utils/udf_util.py
@@ -22,7 +22,21 @@ def extract_class_code(file_content):
 
 
 def extract_salary(file_content):
-    pass
+    try:
+        salary_pattern = r'\$(\d{1,3}(?:,\d{3})+).+?to.+\$(\d{1,3}(?:,\d{3})+)(?:\s+and\s+\$(\d{1,3}(?:,\d{3})+)\s+to\s+\$(\d{1,3}(?:,\d{3})+))?'
+        salary_match = re.search(salary_pattern, file_content)
+
+        if salary_match:
+            salary_start = float(salary_match.group(1).replace(",", ""))
+            salary_end = float(salary_match.group(4).replace(",", "")) if salary_match.group(4) \
+                else salary_match.group(2).replace(",", "")
+        else:
+            salary_start, salary_end = None, None
+
+        return salary_start, salary_end
+
+    except Exception as e:
+        raise ValueError(f"Error extracting salary: {e}")
 
 
 def extract_requirements(file_content):
