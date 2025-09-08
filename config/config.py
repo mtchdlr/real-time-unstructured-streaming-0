fa86d219cd24807d10119a2c18e05186981a3593
mtchdlr <datomtchedluri@gmail.com>
2025-09-09 00:02:52 +0400

    feat(config/config.py): list all desired columns

--- config/config.py
+++ config/config.py
@@ -1,6 +1,12 @@
 import os
+from typing import (Dict
+                    , List)
 
-configuration = {
+configuration: Dict[str, str] = {
     "AWS_ACCESS_KEY_ID": os.getenv("AWS_ACCESS_KEY_ID")
     , "AWS_SECRET_ACCESS_KEY": os.getenv("AWS_SECRET_ACCESS_KEY")
 }
+
+column_list: List[str] = ["file_name", "start_date", "end_date", "salary_start", "salary_end", "classcode"
+                          , "req", "notes", "duties", "selection", "experience_length"
+                          , "education_length", "application_location"]
