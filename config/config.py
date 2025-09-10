b8673b797111c0a7f30e9073500698aed76bcfcc
mtchdlr <datomtchedluri@gmail.com>
2025-09-10 21:35:00 +0400

    chore(config/config.py): move input directory path to the configs

--- config/config.py
+++ config/config.py
@@ -10,3 +10,11 @@ configuration: Dict[str, str] = {
 column_list: List[str] = ["file_name", "start_date", "end_date", "salary_start", "salary_end", "classcode"
                           , "req", "notes", "duties", "selection", "experience_length"
                           , "education_length", "application_location"]
+
+input_dir: str = "file:///home/mtchdlr/real-time-unstructured-streaming-0/input"
+text_input: str = input_dir + "/input_text"
+json_input: str = input_dir + "/input_json"
+video_input: str = input_dir + "/input_video"
+image_input: str = input_dir + "/input_image"
+pdf_input: str = input_dir + "/input_pdf"
+csv_input: str = input_dir + "/input_csv"
